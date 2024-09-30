438591bb0441bd0198e25ede4f11676148d3380a
inca <boris@okunskiy.name>
2024-09-30 15:31:31 +0300

    chore: drop redundant import

--- src/main/app.ts
+++ src/main/app.ts
@@ -8,7 +8,6 @@ import { MainHttpServer } from './global/MainHttpServer.js';
 import { ModuleResolver } from './global/ModuleResolver.js';
 import { UptimeChecker } from './global/UptimeChecker.js';
 import { enableSandbox } from './sandbox.js';
-import { StructuredLogFormatter } from '@nodescript/logger';
 
 export class App extends BaseApp {
 

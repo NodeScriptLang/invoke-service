0dcb82f69a3c2252f5fd07be44220d4faef1e4c0
Boris Okunskiy <boris@okunskiy.name>
2024-11-21 14:08:26 +0300

    chore: use mean max uptime (#2)

--- src/main/global/UptimeChecker.ts
+++ src/main/global/UptimeChecker.ts
@@ -6,19 +6,26 @@ import { dep } from 'mesh-ioc';
 
 export class UptimeChecker {
 
-    @config({ default: 24 * 60 * 60 }) MAX_UPTIME_SECONDS!: number;
+    @config({ default: 24 * 60 * 60 }) MAX_MEAN_UPTIME_SECONDS!: number;
 
     @dep() private logger!: Logger;
 
     startedAt = Date.now();
 
+    // The actual uptime is calculated as +/- 25% of configured mean uptime
+    maxUptimeMs = randomRange(.75, 1.25) * this.MAX_MEAN_UPTIME_SECONDS * 1000;
+
     @statusCheck()
     checkUptime() {
-        if (Date.now() > this.startedAt + this.MAX_UPTIME_SECONDS * 1000) {
+        if (Date.now() > this.startedAt + this.maxUptimeMs) {
             this.logger.warn('Max uptime reached');
             throw new ServerError('Max uptime reached');
         }
         return 'ok';
     }
 
+}
+
+function randomRange(min: number, max: number) {
+    return Math.random() * (max - min) + min;
 }
